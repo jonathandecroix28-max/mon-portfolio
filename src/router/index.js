128d52f96d2497641d8208617aae885148d502ee
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-04-22 09:05:56 +0200

    ajout de novelle route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/HomeView.vue";
 
 import Contact from "../views/ContactView.vue";
+import Projects from "../views/ProjectsView.vue";
+import About from "../views/AboutView.vue";
+import Error404 from "../views/Error404View.vue";
 
 const routes = [
     {
@@ -14,6 +17,21 @@ const routes = [
         path: "/contact",
         name: "Contact",
         component: Contact
+    },
+    {
+        path: "/projects",
+        name: "Projects",
+        component: Projects
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: About
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "NotFound",
+        component: Error404
     }
 ];
 
